1fdb5f1ab06f69645de7b9c3d42c63e71687c95e
drjavab <maghami.rad1@gmail.com>
2023-05-22 13:22:52 +0330

    add port for vandar

--- src/Listeners/SendPaymentCreateRequest.php
+++ src/Listeners/SendPaymentCreateRequest.php
@@ -15,6 +15,7 @@ class SendPaymentCreateRequest
     {
         $payload = $event->payment->only(['amount', 'mobile_number', 'factor_number', 'description', 'valid_card_number']);
         $payload['api_key'] = config('vandar.api_key');
+        $payload['port'] = config('vandar.port');
         $payload['callback_url'] = config('vandar.callback_url');
         $payload['factorNumber'] = $payload['factor_number'];
         unset($payload['factor_number']);
